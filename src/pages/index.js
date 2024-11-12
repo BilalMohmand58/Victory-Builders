6582a47fa71679d61174feca3de09583c26ba471
AbdlReman <abdulrehmanabbasi5067794@gmail.com>
2024-11-12 16:06:34 +0500

    commingsoonpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,7 +44,7 @@ function HomePage({
     return (
         <>
             <Head>
-                <title>Home - VictoryBuilder</title>
+                <title>Home - VictoryBuilders</title>
                 <meta
                     name="description"
                     content="VictoryBuilder - Construction React Next JS Template Industry, Products Manufacturing Company, building companies, architecture firms, and the like can take to their advantage by using OxyBuild - Construction React Next JS Template."
